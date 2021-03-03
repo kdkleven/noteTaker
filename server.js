4172b47b34f589cf58dbee3755fa2425d4dcf0a2
SageTengu <kyle.kleven@outlook.com>
2021-03-02 18:30:37 -0600

    Cleaned up project folder structure by deleting unnecessary Develop folder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 // Tells node that we are creating an "express" server
 var app = express();
 // Sets an initial port.
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8081;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
