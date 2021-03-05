0f81dac4361d25acdc7ec75965921e14419ccc60
SageTengu <kyle.kleven@outlook.com>
2021-03-05 02:27:05 -0600

    Troubleshooting delete note route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,54 +6,75 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
 module.exports = function (app) {
-// GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.    
+    // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.    
     app.get('/api/notes', function (req, res) {
-    // read the json file
+        // read the json file
         fs.readFile(`./db/db.json`, (err, data) => {
             if (err) throw err;
-        // write the header with status, type and format
+            // write the header with status, type and format
             res.writeHead(200, { 'Content-Type': 'text/json' });
-        // write data to the page
+            // write data to the page
             res.write(data);
-        // end the response
+            // end the response
             res.end();
         });
     });
 
-// Receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+    // Receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
     app.post('/api/notes', function (req, res) {
-    // read the json file
+        // read the json file
         fs.readFile(`./db/db.json`, (err, data) => {
-        //declare a variable that parses the read data
+            //declare a variable that parses the read data
             const json = JSON.parse(data);
-        // declare variable that contains a unique/random alphanumeric string
+            // declare variable that contains a unique/random alphanumeric string
             const id = uuidv4();
-        // set a new key in the note with the random id
-            req.body.id = id;
-        // update the existing note data with the id
+            // set a new key in the note with the random id
+            const rb = req.body;
+            rb.id = id;
+            // update the existing note data with the id
             json.push(req.body);
-        // write the updated note data to the json file, and clean up the json format
+            console.log("Pushed JSON:");
+            console.log(json);
+            // write the updated note data to the json file, and clean up the json format
             fs.writeFile(`./db/db.json`, JSON.stringify(json, null, 1), (err) => {
                 if (err) throw err;
-            // end the response
+                // end the response
                 res.end();
             });
             res.end();
         });
     });
 
-// Receive a query parameter containing the id of a note to delete.
-    app.delete('/api/notes/:id', function (req, res) {   
-    // declare a variable that searches through each item in the note array and locates the matching value  
-        const note = db.findIndex(note => note.id === req.params.id);
-    // delete the matched value based on the found index variable
-        db.splice(note, 1);
-    // write the updated array (sans matched index) to the json file
-        fs.writeFile(`./db/db.json`, JSON.stringify(db, null, 1), (err) => {
-            if (err) throw err;
-            res.end();
+    // Receive a query parameter containing the id of a note to delete.
+    app.delete('/api/notes/:id', function (req, res) {
+        console.log("req:");
+        console.log(req.params); 
+        
+        fs.readFile(`./db/db.json`, (err, data) => {
+            //declare a variable that parses the read data
+            console.log("data:");
+            console.log(data);
+            const json = JSON.parse(data);
+            console.log("parsed json:");
+            console.log(json);
+            console.log("req.params.id:");
+            console.log(req.params.id);          
             
-        }); 
-        res.end();
+            // declare a variable that searches through each item in the note array and locates the matching value          
+            const index = db.filter(note => note.id !== req.params.id);
+            console.log("index:");
+            console.log(index);                
+            // update the array with the remaining notes sans deleted note
+            
+            // write the updated note data to the json file, and clean up the json format
+            fs.writeFile(`./db/db.json`, JSON.stringify(index, null, 1), (err) => {
+                if (err) throw err;
+                console.log("Note deleted");
+                // end the response
+                res.end();
+            });
+            res.end();
+        });
     });
+
 };
